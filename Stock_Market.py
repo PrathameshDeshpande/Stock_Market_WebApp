72b64e4f4241e184d5efc833f15d214449aaa3e9
PrathameshDeshpande <60612588+PrathameshDeshpande@users.noreply.github.com>
2020-08-20 20:42:40 +0530

    Files Added

--- Stock_Market.py
+++ Stock_Market.py
@@ -7,14 +7,14 @@ import datetime
 from pandas import DataFrame
 import plotly.express as px
 import plotly.graph_objects as go
-st.title("STOCK MARKET")
-st.write("## Made by Prathamesh with ❤️")
+st.markdown("<h1 style='text-align: center; color: black;'>📈💰STOCK MARKET ANALYSIS🤑📉</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>Made by Prathamesh with ❤️</h1>", unsafe_allow_html=True)
 st.write("### Write The Name Of A Stock You Want To Study 👇👇")
 stock=st.text_input("NAME OF THE STOCK :", "")
 st.write("### Please Select Graph Style Below 👇👇")
-graph_type=st.selectbox("Select Graph Type",("Line Graph","Candle Graph","Both (Line & Bar Graph)"))
+graph_type=st.selectbox("Select Graph Type 📊",("Line Graph","Candle Graph","Both (Line & Bar Graph)"))
 st.write("### Please Select The Category Below 👇👇")
-category =st.selectbox("Select Category Type",("TIME_SERIES_INTRADAY","TIME_SERIES_DAILY"))
+category =st.selectbox("Select Category Type",("TIME_SERIES_INTRADAY","TIME_SERIES_DAILY","TIME_SERIES_MONTHLY"))
 b=st.button("SUBMIT")
 
 def _max_width_():
@@ -37,6 +37,8 @@ def MAIN(stock,graph_type,category):
         TIME_SERIES_INTRADAY(stock,graph_type)
     elif category == "TIME_SERIES_DAILY":
         TIME_SERIES_DAILY(stock,graph_type)
+    elif category == "TIME_SERIES_MONTHLY":
+        TIME_SERIES_MONTHLY(stock,graph_type)
 
 
 def TIME_SERIES_INTRADAY(stock,graph_type):
@@ -96,8 +98,8 @@ def TIME_SERIES_INTRADAY(stock,graph_type):
 def TIME_SERIES_DAILY(stock,graph_type):
     url_a = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='
     url_b = '&apikey=B7M49ARQFBP3BKBF'
-    TIME_SERIES_INTRADAY_url = url_a + stock + url_b
-    data = requests.get(TIME_SERIES_INTRADAY_url).json()
+    TIME_SERIES_DAILY_url = url_a + stock + url_b
+    data = requests.get(TIME_SERIES_DAILY_url).json()
     open = []
     close = []
     high = []
@@ -140,6 +142,60 @@ def TIME_SERIES_DAILY(stock,graph_type):
         fig.update_layout(title='IRR', autosize=False,
                   width=1400, height=800,
                   margin=dict(l=40, r=40, b=40, t=40))
+        st.plotly_chart(fig)
+        fig = px.line(df, x='time_stamp', y='high')
+        fig.update_layout(title='IRR', autosize=False,
+                  width=1400, height=700,
+                  margin=dict(l=40, r=40, b=40, t=40))
+        st.plotly_chart(fig)
+def TIME_SERIES_MONTHLY(stock,graph_type):
+    url_a = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol='
+    url_b = '&apikey=B7M49ARQFBP3BKBF'
+    TIME_SERIES_MONTHLY_url = url_a + stock + url_b
+    data = requests.get(TIME_SERIES_MONTHLY_url).json()
+    open = []
+    close = []
+    high = []
+    low = []
+    volume = []
+    time_stamp = []
+    for i, j in data["Monthly Time Series"].items():
+        time_stamp.append(i)
+        open.append(j["1. open"])
+        close.append(j["4. close"])
+        high.append(j["2. high"])
+        volume.append(j["5. volume"])
+        low.append(j["3. low"])
+    df = DataFrame(list(zip(time_stamp, open, close, high, low, volume)),
+                   columns=['time_stamp', 'open', 'close', 'high', 'low', 'volume'])
+    df["time_stamp"] = df['time_stamp'].astype('datetime64[ns]')
+    if graph_type == "Candle Graph":
+        fig = go.Figure(data=[go.Candlestick(x=df['time_stamp'],
+                                         open=df['open'],
+                                         high=df['high'],
+                                         low=df['low'],
+                                         close=df['close'])])
+
+        fig.update_layout(title='IRR', autosize=False,
+                  width=1400, height=800,
+                  margin=dict(l=40, r=40, b=40, t=40))
+        st.plotly_chart(fig)
+    elif graph_type == "Line Graph":
+        fig = px.line(df, x='time_stamp', y='high')
+        fig.update_layout(title='IRR', autosize=False,
+                  width=1400, height=700,
+                  margin=dict(l=40, r=40, b=40, t=40))
+        st.plotly_chart(fig)
+    elif graph_type == "Both (Line & Bar Graph)":
+        fig = go.Figure(data=[go.Candlestick(x=df['time_stamp'],
+                                         open=df['open'],
+                                         high=df['high'],
+                                         low=df['low'],
+                                         close=df['close'])])
+        fig.update_layout(title='IRR', autosize=False,
+                  width=1400, height=800,
+                  margin=dict(l=40, r=40, b=40, t=40))
+        st.plotly_chart(fig)
         fig = px.line(df, x='time_stamp', y='high')
         fig.update_layout(title='IRR', autosize=False,
                   width=1400, height=700,
